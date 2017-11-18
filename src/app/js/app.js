b3a33267df442032b3783069c499ca2eb8690398
lmcjt37 <lmcjt@outlook.com>
2017-11-18 15:56:14 +0000

    :construction: updated images, refactored code, WIP

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-         .register('./sw.js')
-         .then(function() { console.log('Service Worker Registered'); });
+        .register('./sw.js')
+        .then(function() {
+            console.log('Service Worker Registered'); 
+        });
 }
--- src/app/sw.js
+++ src/app/sw.js
@@ -1,33 +1,30 @@
-var cacheName = 'pwa-commits-v3';
-
-var filesToCache = [
-    './',
-    './css/style.css',
-    './images/books.png',
-    './images/Home.svg',
-    './images/ic_refresh_white_24px.svg',
-    './images/profile.png',
-    './images/push-off.png',
-    './images/push-on.png',
-    './js/app.js',
-    './js/menu.js',
-    './js/offline.js',
-    './js/toast.js'
-];
+var cacheName = 'pwa-example';
 
 // Install Service Worker
 self.addEventListener('install', function(event) {
 
-    console.log('Service Worker: Installing....');
+    console.log('Service Worker: Installing...');
 
     event.waitUntil(
 
         // Open the Cache
         caches.open(cacheName).then(function(cache) {
-            console.log('Service Worker: Caching App Shell at the moment......');
+            console.log('Service Worker: Caching...');
 
             // Add Files to the Cache
-            return cache.addAll(filesToCache);
+            return cache.addAll([
+                './',
+                './css/style.css',
+                './images/home.svg',
+                './images/ic_refresh_white_24px.svg',
+                './images/profile.png',
+                './images/push-off.png',
+                './images/push-on.png',
+                './js/app.js',
+                './js/menu.js',
+                './js/offline.js',
+                './js/toast.js'
+            ]);
         })
     );
 });
@@ -36,13 +33,13 @@ self.addEventListener('install', function(event) {
 // Fired when the Service Worker starts up
 self.addEventListener('activate', function(event) {
 
-    console.log('Service Worker: Activating....');
+    console.log('Service Worker: Activating...');
 
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(cacheNames.map(function(key) {
-                if( key !== cacheName) {
-                    console.log('Service Worker: Removing Old Cache', key);
+                if (key !== cacheName) {
+                    console.log('Service Worker: Clearing Cache... ', key);
                     return caches.delete(key);
                 }
             }));
@@ -54,9 +51,7 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
 
-    console.log('Service Worker: Fetch', event.request.url);
-
-    console.log("Url", event.request.url);
+    console.log('Service Worker: Fetching... ', event.request.url);
 
     event.respondWith(
         caches.match(event.request).then(function(response) {
